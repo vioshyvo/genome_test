4b26e61f28b06704cfe01a33e37f0707ba3ef492
Ville Hyvonen <ville.o.hyvonen@gmail.com>
2017-04-24 10:35:08 +0300

    Fixed slurm wrapper for binary_writer.

--- wrapper-SLURM/writer_slurm.sh
+++ wrapper-SLURM/writer_slurm.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 #SBATCH --time=01:00:00
-#SBATCH --mem=50G
+#SBATCH --mem=30G
 #SBATCH --cpus-per-task=1
 
-#SBATCH -e errors_writer_$SLURM_JOB_ID.txt
+#SBATCH -e errors_writer.txt
 #SBATCH --mail-type=END
 #SBATCH --mail-user=ville.o.hyvonen@helsinki.fi
 
 BASE_DIR="$WRKDIR/Sanger/genome_test"  # set to the path of the repo
 
 if [ "$#" -ne "3" ]; then
-   echo "error: Expecting parameters: <input file> <n_test> <n_train>"
-   echo "error: Expecting parameters: <input file> <n_test> <n_train>" > /dev/stderr
+   echo "error: Expecting parameters: <input file> <n_train> <n_test>"
+   echo "error: Expecting parameters: <input file> <n_train> <n_test>" > /dev/stderr
    exit 1
 fi
 
 
 # move data and binary_writer into the local disc of the node
-mkdir -p "/tmp/$SLURM_JOB_ID/$1"
-cp -a "$BASE_DIR/data/$1/$1.mat" "$BASE_DIR/binary_writer/binary_writer" "/tmp/$SLURM_JOB_ID/$1/"
+mkdir -p "/tmp/$SLURM_JOB_ID"
+cp -a "$BASE_DIR/data/$1/$1.mat" "$BASE_DIR/binary_writer/binary_writer" "/tmp/$SLURM_JOB_ID/"
+cd "/tmp/$SLURM_JOB_ID"
 
-srun binary_writer "$1" "/tmp/$SLURM_JOB_ID/$1/$1.mat" "/tmp/$SLURM_JOB_ID/$1/" $2 $3 > dimensions.sh
+srun ./binary_writer "$1" "$1.mat" "/tmp/$SLURM_JOB_ID/" $2 $3 > dimensions.sh
 
-cp "/tmp/$SLURM_JOB_ID/$1/train.bin" "/tmp/$SLURM_JOB_ID/$1/test.bin" "/tmp/$SLURM_JOB_ID/$1/dimensions.sh" "$BASE_DIR/data/$1/"
+cp train.bin test.bin dimensions.sh "$BASE_DIR/data/$1/"
