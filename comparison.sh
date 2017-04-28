5896f163716b919badc91b1e294f07c286fe41b0
Ville Hyvonen <ville.o.hyvonen@gmail.com>
2017-04-28 13:52:06 +0300

    Merge exact and mrpt test scripts.

--- comparison.sh
+++ comparison.sh
@@ -24,3 +24,10 @@ mkdir -p "results/$DATASET_NAME$ADD"
 for K in 1 10 100; do
     ./exact/tester $N $N_TEST $K $DIM $MMAP "data/$DATASET_NAME/" > "results/$DATASET_NAME$ADD/truth_$K"
 done
+
+echo -n > "results/$DATASET_NAME$ADD/mrpt.txt"
+for n_trees in $MRPT_VOTING_N_TREES; do
+    for depth in $MRPT_DEPTH; do
+        mrpt/mrpt_comparison $N $N_TEST 10 $n_trees $depth $DIM $MMAP "results/$DATASET_NAME$ADD/" "data/$DATASET_NAME/" $MRPT_VOTES  >> "results/$DATASET_NAME$ADD/mrpt.txt"
+    done
+done
