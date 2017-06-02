0e8f9e6c7dcd2aa60c7cfd741219b0e1f8d1709c
Ville Hyvonen <ville.o.hyvonen@gmail.com>
2017-06-02 14:43:50 +0300

    Install cmake as dependency.

--- install_fsm.sh
+++ install_fsm.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+PROGRAM=cmake
+PROGRAM_DIR="cmake-3.8.2-Linux-x86_64"
+PROGRAM_URL="https://cmake.org/files/v3.8"
+if [ $(command -v "$PROGRAM") ]; then
+  echo Checking dependencies: "$PROGRAM" is installed.
+else
+  if [ -d "$PROGRAM_DIR" ]; then
+    echo Checking dependencies: "$PROGRAM" is installed in "$PROGRAM_DIR", but not in path.
+  else
+    echo Checking dependencies: "$PROGRAM" is not installed.
+    echo Installing "$PROGRAM" into "$PROGRAM_DIR"...
+    wget "$PROGRAM_URL/$PROGRAM_DIR.tar.gz"
+    tar -zxf "$PROGRAM_DIR.tar.gz"
+    rm "$PROGRAM_DIR.tar.gz"
+  fi
+  echo Adding directory "$PROGRAM_DIR" into path.
+  CRNT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+  export PATH="$PATH:$CRNT_DIR/$PROGRAM_DIR/bin"
+fi
+
+
 if [ ! -d sdsl-lite ]; then
   git clone https://github.com/simongog/sdsl-lite.git
   pushd sdsl-lite
