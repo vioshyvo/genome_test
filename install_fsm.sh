b5d20311a525179802b1b6e186456ba1c3aca5f5
Ville Hyvonen <ville.o.hyvonen@gmail.com>
2017-06-20 12:47:19 +0300

    Add eigen to installer

--- install_fsm.sh
+++ install_fsm.sh
@@ -46,3 +46,13 @@ if [ ! -d fsm-lite ]; then
 else
   echo fsm-lite is already installed.
 fi
+
+if [ ! -d eigen ]; then
+  echo Installing Eigen.
+  wget http://bitbucket.org/eigen/eigen/get/3.3.4.tar.bz2 -P tmp/
+  mkdir eigen
+  tar xfj tmp/3.3.4.tar.bz2 -C eigen --strip-components=1
+  echo Eigen installed.
+else
+  echo Eigen 3.3.4 already installed
+fi
