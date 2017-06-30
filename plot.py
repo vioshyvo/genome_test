b3a66d3a79d54acf20162ec2919032bb6370967c
Ville Hyvonen <ville.o.hyvonen@gmail.com>
2017-06-30 09:48:35 +0300

    Add exact time for plotting, and plot times per query point (not total time)

--- plot.py
+++ plot.py
@@ -32,20 +32,25 @@ def pareto_frontier(Xs, Ys, maxX=True, maxY=True):
 
 
 def main(k, files):
-    legend = True
+    n_test = 50
+    legend = False
     save = False
     log = False
     set_ylim = True
-    legend_label = 'depth' # 'sparsity' or 'depth'
+    legend_label = 'sparsity' # 'sparsity' or 'depth'
+    show_title = True
 
-    ylim = (0,100)
-    file_name = 'images/depth.png'
+    ylim = (0,100 / n_test)
+    file_name = 'images/3depth.png'
+    title = 'Ecol data set, n = 1500, d = 23223411'
+    exact_time = 21 # 50 test set points x approximately 22 seconds
 
     fig = plt.figure()
     LSD = []
     q = False
     A = []
     ax = plt.gca()
+
     for resfile in files:
         with open(resfile) as f:
             _ = f.readline()
@@ -58,19 +63,28 @@ def main(k, files):
     minY, maxY = float('inf'), -float('inf')
     for a, c, m in zip(A, colors, ['>', 'v', 'd', '^', 'o', 'p', 'h', '<']):
         par = pareto_frontier(a[1], a[2], True, False)
-        l, = ax.plot(par[0], par[1], linestyle='solid', marker=m, label=a[0], c=c, markersize=7)
+        query_times = par[1]
+        times_per_point = [x / n_test for x in query_times] # divide by the number of query points
+        l, = ax.plot(par[0], times_per_point, linestyle='solid', marker=m, label=a[0], c=c, markersize=7)
         if q: LSD.append(l)
-        maxY = max(maxY, max(par[1]))
-        minY = min(minY, min(x for x, y in zip(par[1], par[0]) if y >= 0.5))
+        maxY = max(maxY, max(times_per_point))
+        accuracy_time_list = zip(times_per_point, par[0])
+        minY = min(minY, min(x for x, y in accuracy_time_list if y >= 0.5))
     ax.semilogy()
-    ax.set_ylabel('time (s)', fontsize=22)
-    ax.set_xlabel('recall', fontsize=22)
+    ax.set_ylabel('time (s)', fontsize=20)
+    ax.set_xlabel('recall', fontsize=20)
     ax.set_xlim((0, 1))
+
+    for pair in accuracy_time_list:
+        print pair
+
+    if show_title:
+        ax.set_title(title, fontsize=20, y=1.05)
 
     if set_ylim:
         ax.set_ylim(ylim)
     else:
-        ax.set_ylim((minY / 1.25, maxY * 1.25))
+        ax.set_ylim((minY / 1.25, max(maxY * 1.25, exact_time * 1.15)))
 
     ax.xaxis.labelpad = 15
     ax.yaxis.labelpad = 15
@@ -80,17 +94,18 @@ def main(k, files):
     else:
         ax.set_yscale('linear')
 
-    # ax.set_title('title', fontsize=28, y=1.05)
     # ax.set_yticks(np.linspace(0, 1, 100))
 
     legend_idx = 0 if legend_label == 'sparsity' else 3
     if legend:
         ax.legend(LSD, labels=[a[legend_idx] for a in A], loc="upper left", title = legend_label)
+
+    plt.axhline(y = exact_time, xmin = 0, xmax = 1, hold = None, linestyle = '--', color = 'red')
+
     if save:
         plt.savefig(file_name, bbox_inches='tight')
     else:
         plt.show()
-
 
 if __name__ == '__main__':
     main(int(sys.argv[1]), sys.argv[2:])
