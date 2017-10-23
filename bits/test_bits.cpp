152ebfec5dd84abb06633b64e30820572d5f98d3
Ville Hyvonen <ville.o.hyvonen@gmail.com>
2017-10-23 12:30:28 +0300

    Add hand-rolled version distance using vectors

--- bits/test_bits.cpp
+++ bits/test_bits.cpp
@@ -109,9 +109,46 @@ int distance4(const VecI &x, const VecI &y) {
   return (x - y).count();
 }
 
-int distance_basic(std::vector<int> &x, std::vector<int> &y) {
-  return 0;
+int distance_basic(const std::vector<int> &x, const std::vector<int> &y) {
+  int sum = 0;
+  auto iter_x = x.cbegin(), iter_y = y.cbegin();
+  auto x_end = x.cend(), y_end = y.cend();
+  int x_idx = -1, y_idx = -1;
+  if(iter_x != x_end) x_idx = *iter_x;
+  if(iter_y != y_end) y_idx = *iter_y;
+
+  while(iter_x != x_end || iter_y != y_end) {
+    // std::cout << "x: " << iter_x.value() << ", y: " << iter_y.value() << "\n";
+    if(x_idx < y_idx) {
+      // std::cout << "x_idx < y_idx: " << x_idx << " < " << y_idx << "\n";
+      sum += 1;
+      if(iter_x != x_end) ++iter_x; else ++iter_y;
+      if(iter_x != x_end) {
+        x_idx = *iter_x;
+      } else if(iter_y != y_end) {
+        y_idx = *iter_y;
+      }
+    } else if(x_idx > y_idx) {
+      // std::cout << "x_idx > y_idx: " << x_idx << " > " << y_idx << "\n";
+      sum += 1;
+      if(iter_y != y_end) ++iter_y; else ++iter_x;
+      if(iter_y != y_end) {
+        y_idx = *iter_y;
+      } else if(iter_x != x_end) {
+        x_idx = *iter_x;
+      }
+    } else {
+      // std::cout << "x_idx == y_idx: " << x_idx << " == " << y_idx << "\n";
+      if(iter_x != x_end) ++iter_x;
+      if(iter_y != y_end) ++iter_y;
+      if(iter_x != x_end) x_idx = *iter_x;
+      if(iter_y != y_end) y_idx = *iter_y;
+    }
+  }
+
+  return sum;
 }
+
 
 int project(boost::dynamic_bitset<> x, boost::dynamic_bitset<> rv_plus, boost::dynamic_bitset<> rv_minus) {
   return (x & rv_plus).count() - (x & rv_minus).count();
