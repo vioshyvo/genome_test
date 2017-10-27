d7b184fa3931e125f0b8124fb33050c3ce6148e5
Ville Hyvonen <ville.o.hyvonen@gmail.com>
2017-10-27 11:55:13 +0300

    Generate dimensions - file in a script that loads mnist data

--- bits/mrpt_test/mrpt_test.cpp
+++ bits/mrpt_test/mrpt_test.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <string>
 #include <Eigen/Dense>
+#include <omp.h>
+#include "cpp/Mrpt.h"
+#include "common.h"
 
 int main(int argc, char** argv) {
+  
+
+  std::string base_path("../..");
+  std::string path(base_path + "/data/mnist");
+  std::cout << base_path << std::endl;
+  std::cout << path << std::endl;
+  float *train_data = get_data((std::string(path) + "/train.bin").c_str(), dim, &n);
+  float *test_data = get_data((std::string(path) + "/test.bin").c_str(), dim, &n_test);
   std::cout << "Moi!" << std::endl;
   return 0;
 }
