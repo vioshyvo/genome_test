56ed0f80f60722685b8ec59ca03f0352111ef1d6
Ville Hyvonen <ville.o.hyvonen@gmail.com>
2017-11-02 16:55:39 +0200

    update slurm wrapper for binary_writer

--- wrapper-SLURM/writer_slurm.sh
+++ wrapper-SLURM/writer_slurm.sh
@@ -3,31 +3,26 @@
 #SBATCH --mem=30G
 #SBATCH --cpus-per-task=1
 
-#SBATCH -e "errors_writer-$SLURM_JOB_ID.txt"
+#SBATCH -e "errors-writer-$SLURM_JOB_ID.txt"
 #SBATCH --mail-type=END
 #SBATCH --mail-user=ville.o.hyvonen@helsinki.fi
 
 BASE_DIR="$WRKDIR/Sanger/genome_test"  # set to the path of the repo
 
-if [ "$#" -ne 4 ] && [ "$#" -ne 5 ]; then
-   echo "error: Expecting parameters: <data-name> <n_train> <n_test> <counts> or <data-name> <n_train> <n_test> <counts> <data-monicker> " 1>&2
+if [ "$#" -ne 4 ]; then
+   echo "error: Expecting parameters: <data-name> <n_train> <n_test> <counts>" 1>&2
    exit
 fi
 
-((N = $2 + $3))
-COUNTS="$5"
-
-if [ "$#" -eq 3 ]; then
-  DATA_NAME="$1$N"
-else
-  DATA_NAME="$1$N-$5"
-fi
+N_TRAIN="$2"
+N_TEST="$3"
+COUNTS="$4"
+DATA_NAME="$1"
 
 DATA_DIR="$BASE_DIR/data/$DATA_NAME"
 DATA_FILE="$DATA_NAME.mat"
 
 TMP_DIR="/tmp/$SLURM_JOB_ID"
-
 
 if [ ! -f "$DATA_DIR/$DATA_FILE" ]; then
   echo File $DATA_DIR/$DATA_FILE does not exist
@@ -39,6 +34,6 @@ mkdir -p "$TMP_DIR"
 cp -a "$DATA_DIR/$DATA_FILE" "$BASE_DIR/binary_writer/binary_writer" "$TMP_DIR"
 cd "$TMP_DIR"
 
-srun ./binary_writer "$DATA_NAME" "$DATA_FILE" "$TMP_DIR" $2 $3 $COUNTS > dimensions.sh
+srun ./binary_writer "$DATA_NAME" "$DATA_FILE" "$TMP_DIR" "$N_TRAIN" "$N_TEST" "$COUNTS" > dimensions.sh
 
 cp train.bin test.bin dimensions.sh "$DATA_DIR"
