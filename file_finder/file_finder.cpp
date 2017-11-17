d3da52ab0fadf652acb2cd06169df659ddb113ec
Ville Hyvonen <ville.o.hyvonen@gmail.com>
2017-11-17 09:35:35 +0200

    Add shell script wrapper for file finder

--- file_finder/file_finder.cpp
+++ file_finder/file_finder.cpp
@@ -2,6 +2,12 @@
 #include <string>
 
 int main(int argc, char **argv) {
+  int n_args = 1;
+  if (argc != n_args + 1) {
+      std::cerr << "Usage: " << argv[0] << " permutations_file" << std::endl;
+      return 1;
+  }
+  
   std::string permutations_file(argv[1]);
   std::cout << "Hi from C++!" << std::endl;
 
