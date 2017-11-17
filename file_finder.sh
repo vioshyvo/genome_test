559cc067e2408fd747f6c52a9061839f8f483858
Ville Hyvonen <ville.o.hyvonen@gmail.com>
2017-11-17 10:32:00 +0200

    Add makefile for file finder

--- file_finder.sh
+++ file_finder.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+
+if [ "$#" -ne 2 ]; then
+   echo "error: Expecting parameters: <data-set-name> <k>" 1>&2
+   exit
+fi
+
+DATA_NAME="$1"
+K="$2"
+PERMUTATIONS_FILE="data/$DATA_NAME/permutations"
+EXACT_FILE="results/$DATA_NAME-exact/truth_$K"
+
+pushd file_finder
+make clean
+make
+popd
+
+file_finder/file_finder "$PERMUTATIONS_FILE" "$EXACT_FILE"
--- file_finder/file_finder.cpp
+++ file_finder/file_finder.cpp
@@ -1,16 +1,43 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 
 int main(int argc, char **argv) {
-  int n_args = 1;
+  int n_args = 2;
   if (argc != n_args + 1) {
-      std::cerr << "Usage: " << argv[0] << " permutations_file" << std::endl;
+      std::cerr << "Usage: " << argv[0] << " permutations_file exact_file" << std::endl;
       return 1;
   }
-  
+
   std::string permutations_file(argv[1]);
+  std::string exact_file(argv[2]);
   std::cout << "Hi from C++!" << std::endl;
 
+  std::ifstream infile(permutations_file);
+  if(!infile) {
+      std::cerr << "Error: could not open file " << permutations_file << "\n";
+      return -1;
+  }
 
+  int i;
+  while(infile) {
+    infile >> i;
+    std::cout << i << " ";
+  }
+  std::cout << std::endl;
+
+  std::ifstream infile_exact(exact_file);
+  if(!infile_exact) {
+      std::cerr << "Error: could not open file " << permutations_file << "\n";
+      return -1;
+  }
+
+  std::string s;
+  while(infile_exact) {
+    infile_exact >> s;
+    std::cout << s << " ";
+  }
+
+  std::cout << std::endl;
   return 0;
 }
