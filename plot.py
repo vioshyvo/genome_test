cedc395044ddacc2fcb0fffba4ef60a1d4116713
Ville Hyvonen <ville.o.hyvonen@helsinki.fi>
2018-10-11 17:36:27 +0100

    Add plotting of index building time

--- plot.py
+++ plot.py
@@ -10,8 +10,8 @@ plt.rcParams['xtick.labelsize'] = 18
 plt.rcParams['ytick.labelsize'] = 18
 
 
-def pareto_frontier(Xs, Ys, maxX=True, maxY=True):
-    myList = sorted([[Xs[i], Ys[i]] for i in range(len(Xs))], reverse=maxX)
+def pareto_frontier(Xs, Ys, index_time = 0, maxX=True, maxY=True):
+    myList = sorted([[Xs[i], Ys[i], index_time[i]] for i in range(len(Xs))], reverse=maxX)
     p_front = [myList[0]]
     for pair in myList[1:]:
         if maxY:
@@ -20,7 +20,7 @@ def pareto_frontier(Xs, Ys, maxX=True, maxY=True):
         else:
             if pair[1] <= p_front[-1][1]:
                 p_front.append(pair)
-    p_frontX, p_frontY = [], []
+    p_frontX, p_frontY, p_front_index = [], [], []
     edX = -1
     for pair in reversed(p_front):
         if pair[0] < 0.1 or pair[0] - edX <= 0.001:
@@ -28,7 +28,8 @@ def pareto_frontier(Xs, Ys, maxX=True, maxY=True):
         edX = pair[0]
         p_frontX.append(pair[0])
         p_frontY.append(pair[1])
-    return p_frontX, p_frontY
+        p_front_index.append(pair[2])
+    return p_frontX, p_frontY, p_front_index
 
 
 def main(k, files):
@@ -36,15 +37,16 @@ def main(k, files):
     legend = True
     save = False
     log = True
-    set_ylim = True
+    set_ylim = False
     legend_label = 'filename' # 'sparsity', 'depth' or 'filename'
     show_title = True
+    build_times = True
 
     # ylim = (0,100 / n_test)
     ylim = (0,.01) # mnist data
     file_name = 'images/depth.png'
     title = 'Ecol data set, n = 1500, d = 23223411'
-    exact_time = 21 # 50 test set points x approximately 22 seconds
+    exact_time = -1 # 50 test set points x approximately 22 seconds
 
     fig = plt.figure()
     LSD = []
@@ -58,19 +60,22 @@ def main(k, files):
             lines = [[float(x) for x in s.strip().split()] for s in f]
         acc = [x[5] for x in lines if x[0] == k]
         tym = [x[7] for x in lines if x[0] == k]
+        index_time = [x[11] for x in lines if x[0] == k] if build_times else np.zeros(len(acc))
         # A.append((resfile.split('.')[0], acc, tym))
         A.append((lines[0][3], acc, tym, lines[0][2]))
     colors = cm.rainbow(np.linspace(0, 1, len(A)))
     minY, maxY = float('inf'), -float('inf')
     for a, c, m in zip(A, colors, ['>', 'v', 'd', '^', 'o', 'p', 'h', '<']):
-        par = pareto_frontier(a[1], a[2], True, False)
+        par = pareto_frontier(a[1], a[2], index_time, True, False)
         query_times = par[1]
+        index_times = par[2]
         times_per_point = [x / n_test for x in query_times] # divide by the number of query points
-        l, = ax.plot(par[0], times_per_point, linestyle='solid', marker=m, label=a[0], c=c, markersize=7)
+        times = index_times if build_times else times_per_point
+        l, = ax.plot(par[0], times, linestyle='solid', marker=m, label=a[0], c=c, markersize=7)
         if q: LSD.append(l)
-        maxY = max(maxY, max(times_per_point))
-        accuracy_time_list = zip(times_per_point, par[0])
-        minY = min(minY, min(x for x, y in accuracy_time_list if y >= 0.5))
+        maxY = max(maxY, max(times))
+        accuracy_time_list = zip(times_per_point, par[0], par[2])
+        minY = min(minY, min(x for x, y, z in accuracy_time_list if y >= 0.5))
     ax.semilogy()
     ax.set_ylabel('time (s)', fontsize=20)
     ax.set_xlabel('recall', fontsize=20)
@@ -85,7 +90,10 @@ def main(k, files):
     if set_ylim:
         ax.set_ylim(ylim)
     else:
-        ax.set_ylim((minY / 1.25, max(maxY * 1.25, exact_time * 1.15)))
+        y_lower_multiplier = 1 if build_times else 5
+        if build_times:
+            minY = min(index_times)
+        ax.set_ylim((minY / y_lower_multiplier, max(maxY * 1.25, exact_time * 1.15)))
 
     ax.xaxis.labelpad = 15
     ax.yaxis.labelpad = 15
@@ -98,11 +106,12 @@ def main(k, files):
     # ax.set_yticks(np.linspace(0, 1, 100))
 
     legend_idx = 0 if legend_label == 'sparsity' else 3
-    labels = files if legend_label == 'filename' else [a[legend_idx] for a in A] 
+    labels = files if legend_label == 'filename' else [a[legend_idx] for a in A]
     if legend:
         ax.legend(LSD, labels=labels, loc="upper left", title = legend_label)
 
-    plt.axhline(y = exact_time, xmin = 0, xmax = 1, hold = None, linestyle = '--', color = 'red')
+    if exact_time > 0:
+        plt.axhline(y = exact_time, xmin = 0, xmax = 1, hold = None, linestyle = '--', color = 'red')
 
     if save:
         plt.savefig(file_name, bbox_inches='tight')
